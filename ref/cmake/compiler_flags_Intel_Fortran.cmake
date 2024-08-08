95b22072e61a2b411733c55ef19974217aa6de72
christopherwharrop-noaa <Christopher.W.Harrop@noaa.gov>
2024-08-08 19:41:17 +0000

    Turn off assume minus0

--- ref/cmake/compiler_flags_Intel_Fortran.cmake
+++ ref/cmake/compiler_flags_Intel_Fortran.cmake
@@ -14,7 +14,7 @@ set( CMAKE_Fortran_FLAGS_RELEASE "-O3 -xHost " )
 # DEBUG FLAGS
 ####################################################################
 
-set( CMAKE_Fortran_FLAGS_DEBUG   "-O0 -debug -nolib-inline -fno-inline-functions -assume protect_parens,minus0 -prec-div -prec-sqrt -check bounds -fp-stack-check -init=snan,array -warn unused" )
+set( CMAKE_Fortran_FLAGS_DEBUG   "-O0 -debug -nolib-inline -fno-inline-functions -assume protect_parens -prec-div -prec-sqrt -check bounds -fp-stack-check -init=snan,array -warn unused" )
 
 ####################################################################
 # FLAGS FOR GPUS
