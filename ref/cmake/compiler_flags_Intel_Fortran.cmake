67b156412bced077aff67d5c41239a690ad49e6f
Christopher Harrop <35781497+christopherwharrop-noaa@users.noreply.github.com>
2024-08-08 13:44:49 -0600

    Reinstate use of debug tests for linux GNU and Intel (#4)
    * Scale forcet to prevent negative values passed to log()
    
    This fixes a numerical bug preventing tests from passing
    in debug mode.  A bug in ifx giving false positives during
    MPI initialization forced turning that debug option off.
    Debug tests are now reinstated.
    
    * Turn off assume minus0

--- ref/cmake/compiler_flags_Intel_Fortran.cmake
+++ ref/cmake/compiler_flags_Intel_Fortran.cmake
@@ -2,7 +2,7 @@
 # COMMON FLAGS
 ####################################################################
 
-set( CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -g -traceback -fp-model precise " )
+set( CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -g -traceback -fp-model precise -assume nominus0" )
 
 ####################################################################
 # RELEASE FLAGS
@@ -14,7 +14,7 @@ set( CMAKE_Fortran_FLAGS_RELEASE "-O3 -xHost " )
 # DEBUG FLAGS
 ####################################################################
 
-set( CMAKE_Fortran_FLAGS_DEBUG   "-O0 -debug -nolib-inline -fno-inline-functions -assume protect_parens,minus0 -prec-div -prec-sqrt -check bounds -check uninit -fp-stack-check -init=snan,array -warn unused" )
+set( CMAKE_Fortran_FLAGS_DEBUG   "-O0 -debug -nolib-inline -fno-inline-functions -assume protect_parens -prec-div -prec-sqrt -check bounds -fp-stack-check -init=snan,array -warn unused" )
 
 ####################################################################
 # FLAGS FOR GPUS
