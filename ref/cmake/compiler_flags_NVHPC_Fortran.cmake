19c2e2e197a809678b903d521244ba0126949185
Christopher Harrop <35781497+christopherwharrop-noaa@users.noreply.github.com>
2024-08-10 11:34:04 -0600

    Add NVHPC CI workflow for cpu (#6)
    * Add first try at NVHPC cpu CI workflow
    
    * Add new cmake files for NVHPC compiler options
    
    * Remove unused code and add VERBOSE build to nvhpc ci workflow
    
    * Add debug step to nvhpc ci
    
    * Remove debug option for nvhpc, appears to be a compiler bug

---
+++ ref/cmake/compiler_flags_NVHPC_Fortran.cmake
@@ -0,0 +1,23 @@
+####################################################################
+# COMMON FLAGS
+####################################################################
+set( CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -g -traceback -Mnofma")
+
+####################################################################
+# RELEASE FLAGS
+####################################################################
+
+# Must turn off SIMD vectorization to get same results as for debug
+set( CMAKE_Fortran_FLAGS_RELEASE "-fast -mp -Mnovect" )
+
+####################################################################
+# DEBUG FLAGS
+####################################################################
+
+set( CMAKE_Fortran_FLAGS_DEBUG "${CMAKE_Fortran_FLAGS_DEBUG} -O0 -Mbounds -Mchkptr -Mchkstk" )
+
+####################################################################
+# FLAGS FOR GPU
+####################################################################
+
+set( Fortran_GPU_FLAGS        "" )
